c7baafe8288978f1950f13624eb665923cd7ec15
P Naga <leadernaga90@gmail.com>
2022-06-07 18:46:52 +0530

    Update headerSocials.jsx

--- src/Components/Header/headerSocials.jsx
+++ src/Components/Header/headerSocials.jsx
@@ -7,7 +7,7 @@ const headerSocials = () => {
   return (
       <div className="header__socials">
           <a href="https://www.linkedin.com/in/pnaga15/" target="__blank"><BsLinkedin/></a>
-          <a href="http://github.com.com/leadernaga" target="__blank"><FaGithub/></a>
+          <a href="http://github.com/leadernaga" target="__blank"><FaGithub/></a>
           <a href="http://facebook.com" target="__blank"><FaFacebook/></a>
     </div>
   )
