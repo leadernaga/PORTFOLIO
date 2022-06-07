2f85378b0a2266109c94bcebc6f92f7bc58b38a6
P Naga <leadernaga90@gmail.com>
2022-06-07 17:57:16 +0530

    tool tip added to navbar

--- src/Components/Navbar/Nav.jsx
+++ src/Components/Navbar/Nav.jsx
@@ -10,7 +10,22 @@ import 'tippy.js/dist/tippy.css';
 export const Nav = () => {
   const [state, setState] = useState("#");
   tippy('#hm', {
-  content: "home",
+    content: "home",
+    animation: "scale",
+    theme: 'light',
+
+  });
+  tippy('#ab', {
+  content: "about",
+  });
+  tippy('#exp', {
+  content: "skills",
+  });
+  tippy('#prj', {
+  content: "projects",
+  });
+  tippy('#cntct', {
+  content: "contact",
 });
   
   
@@ -20,20 +35,20 @@ export const Nav = () => {
       <a id="hm" data-tip data-for='ho' href="#" onClick={() => setState("#")} className={state === "#" ? "active" : ""}><AiOutlineHome /></a>
         
 
-      <a id="m" href="#about" onClick={() => setState("about")} className={state === "about" ? "active" : ""}><AiOutlineUser />
+      <a id="ab" href="#about" onClick={() => setState("about")} className={state === "about" ? "active" : ""}><AiOutlineUser />
       
       </a>
-      <a href="#experience" onClick={() => setState("experience")} className={state === "experience" ? "active" : ""}>< BiBook />
+      <a id="exp" href="#experience" onClick={() => setState("experience")} className={state === "experience" ? "active" : ""}>< BiBook />
 
         
       </a>
       
 
-      <a  href="#portfolio" onClick={() => setState("services")} className={state === "services" ? "active" : ""}><RiServiceLine />
+      <a id="prj"   href="#portfolio" onClick={() => setState("services")} className={state === "services" ? "active" : ""}><RiServiceLine />
       
       
       </a>
-      <a data-tip data-for='cntct' href="#contact" onClick={() => setState("contact")} className={state === "contact" ? "active" : ""}><BiMessageSquareDetail />
+      <a id="cntct" href="#contact" onClick={() => setState("contact")} className={state === "contact" ? "active" : ""}><BiMessageSquareDetail />
 
       </a>
           
